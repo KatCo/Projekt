0cfa92711195db7478e98b47d1f7aaf55c1858f5
IngaF <inga.fuhrmann@gmail.com>
2015-10-27 10:58:15 +0100

    Login

---
+++ login2.php
@@ -0,0 +1,24 @@
+<?php 
+
+	session_start(); //später mit MySQL
+	
+	$_logindaten = Array("name"=>"inga", "passwort"=>"12345"); //Speichern im Array, wie die Logindaten lauten müssten
+	if (isset($_POST["username"]) && isset($_POST["password"])) //Bedingung: Die Variablen aus dem Formular müssen eingegeben sein
+		{
+			if ($_logindaten["name"] == $_POST["username"] &&	//Bedingung: Die eingebenen Daten müssen mit den 
+				$_logindaten["passwort"] == $_POST["password"]) //vorgegebenen Daten übereinstimmen
+				{
+					$_SESSION["login"] = 1; //Sind die Daten richtig, wird der User eingeloggt
+					echo("Du bist eingeloggt!");
+					include("home.html");
+				}
+			else 
+			{
+				echo ("Anmeldung fehlgeschlagen. Versuche es erneut!");
+				include("login.html");
+				exit;
+			}
+		}
+//$username = $_POST ["username"];
+//echo($username.", du bist eingeloggt!");
+?>
