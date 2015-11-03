f97a0da369fee64354ae259828cb5864815210f9
IngaF <inga.fuhrmann@gmail.com>
2015-11-03 09:29:26 +0100

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -2,13 +2,13 @@
 <?php
 /*Verbindung mit Datenbank herstellen
  *da die Datenbankverbindung in mehereren Dateien verwendet wird, wird die dbconnect.php in die
- *jeweiigen Datein eingebunden 
+ *jeweiligen Dateien eingebunden 
  *vor <html> steht dann: 
  *<?php
  *include("dbconnect.php");
  ?>*/
 
-	$db = mysqli_connect("localhost", "Benutzername", "Passwort", "Datenbankname"); //Erg‰nzung von Servernamen, Benutzernamen, Passwort und Datenbank!
+	$db = mysqli_connect("localhost", "Benutzername", "Passwort", "Datenbankname"); //Erg√§nzung von Servernamen, Benutzernamen, Passwort und Datenbank!
 	if(!$db)				
 	{
 		exit("Fehler: ".mysqli_connect_error()); //tritt ein Fehler auf, erscheint der Fehlercode
