26fb131eb8906c43d71250e40a611fe89b03202d
IngaF <inga.fuhrmann@gmail.com>
2015-11-10 11:34:23 +0100

    Datumsfunktion

--- Datumsfunktion.php
+++ Datumsfunktion.php
@@ -1,1 +1,8 @@
 <?php
+//Datum zum Einbinden
+setlocale(LC_TIME, "de"); //Festlegen des Sprach-/ und Gebietschemas auf Deutschland
+echo "Heute ist ";
+echo strftime("%A"); // Ausgeschriebener Name des Wochentages
+echo " , der ";
+echo strftime ("%x". "."); //Wiedergabe des Datums ohne Zeitangabe
+?>
