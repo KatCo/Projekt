1849a0123701c5e58bd8be366a4934b58fe619ab
IngaF <inga.fuhrmann@gmail.com>
2015-11-10 11:17:21 +0100

    Update Dateiabfragen

--- Dateiabfragen.php
+++ Dateiabfragen.php
@@ -1,1 +1,4 @@
 <?php
+$abfrage = "SELECT* FROM csvformular.csv"; //Abfrage in einer Variable speichern
+$ergebnis = mysqli_query($db, $abfrage); //Ausführung der Abfrage und Speiccherung in einer Variablen
+?>
