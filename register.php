4d249f47b8e48c75dab1f9eddc8322c4b7acdd89
IngaF <inga.fuhrmann@gmail.com>
2015-11-17 11:15:15 +0100

    Registrierung

--- register.php
+++ register.php
@@ -7,16 +7,30 @@ $pass2 = $_POST["pass2"];
 
 //Überprüfen, ob die eingetragenen Passwörter identisch sind
 if ($pass == $pass2) {
-	$user_2 = array();	//Wenn sie identisch sind, überprüfen, ob Benutzername schon einmal verwendet wurde ==>leeres Array
+	$name_2 = array();	//Wenn sie identisch sind, überprüfen, ob Benutzername schon einmal verwendet wurde ==>leeres Array
 	$pass = md5($pass); //Verschlüsselung des Passwortes
 	
 	$benutzerdatei = fopen ("benutzer.txt", "r"); //öffnen der Datei mit den Benutzernamen
 	while (!feof($benutzerdatei)) {				// Datei wird Stück für Stück ausgelesen
 		$zeile = fgets($benutzerdatei, 500);
-		$data = explode("|", $zeile);
-		array_data ($user_2, $data[0]);			//Der Benutzername wird ins Array abgelegt
+		$data = explode(";", $zeile);
+		array_data ($name_2, $data[0]);			//Der Benutzername wird ins Array abgelegt
 	}
 	fclose($benutzerdatei); //Datei wird wieder geschlossen
+	
+	if (in_array($name, $name_2)) { //Ist der Benutzername schon vorhanden, erscheint ein Hinweis
+		echo "Der Benutzername ist leider schon vorhanden. Bitte gib´ einen neuen Namen ein!";
+		include ("form.html");
+	}
+	else {
+		$eintrag = "$name ; $pass"; //Name und Passwort werden mit einem ";" getrennt und in eine Variable gespeichert
+		$benutzerdatei = fopen ("benutzer.txt", "a");
+		fwrite($benutzrdatei, "$eintrag\n");
+		fclose($benutzerdatei);
+		
+		echo $name.", deine Anmeldung war erfolgreich! Jetzt kannst du loslegen! ";
+	}
+	
 }
 else {
 	//Stimmen die Passwörter nicht überein, erscheint ein Hinweis
