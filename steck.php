91d02e34c3107d33de9e71393d1c0409cf6dddf9
IngaF <inga.fuhrmann@gmail.com>
2015-11-30 13:05:38 +0100

    Steckbriefe
    Ausgabe der Namen der angelegten Steckbriefe mit Logo

--- steck.php
+++ steck.php
@@ -6,17 +6,27 @@ $csv = array(); 						//anlegen eines Arrays
 while(($data = fgetcsv($datei, 1000, ";")) !== FALSE) { //while-schleife zum befüllen des Arrays
 	 $csv[] = $data;					//Mehrdimensionales Array
 	//var_dump($data); 					//Inhalte des Arrays anzeigen
+
+			
+			
 }
-			//Ausgabe durch Ansprechen des entsprechenden Array-Indexes/ Erste Stelle bezieht sich auf den Datensatz/ zweite Stelle auf jeweilige Variable im Datensatz
-			//Hier fehlt noch die Schleife!
-			echo "<p>Steckbrief</p>";
+
+foreach ($csv as list($csv2)) { //Auslesen der ersten Variable des jeweiligen Datensatzes
+//	foreach ($csv2 as $csv3) { //Auslesen des kompletten Arrays
+		echo "HTML-CODE mit Logo"; //Ausgeben eines Logos
+		echo	"<p>$csv2</p>"; //Ausgeben der ersten Variable -> Name
+		
+	}
+
+//Ausgabe durch Ansprechen des entsprechenden Array-Indexes/ Erste Stelle bezieht sich auf den Datensatz/ zweite Stelle auf jeweilige Variable im Datensatz
+/*			echo "<p>Steckbrief</p>";
 			echo "<p>Name: ".$csv[0][0]."</p>";
 			echo "<p>Geburtstag: ".$csv[0][1]."</p>";
-			echo "Interessen: ".$csv[0][2]."</p>";
-			echo "Anmerkungen: ".$csv[0][3]."</p>";
+			echo "<p>Interessen: ".$csv[0][2]."</p>";
+			echo "<p>Anmerkungen: ".$csv[0][3]."</p>";
 	
     
-
+*/
  fclose($datei); // Schließen der Datei
 
   
