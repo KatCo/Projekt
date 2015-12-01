d9e668f4e88ac6ce295999f0bfd43f191b39fe5a
IngaF <inga.fuhrmann@gmail.com>
2015-12-01 12:35:40 +0100

    Steckbrief
    Übermittlung der Namenvariable

--- steck.php
+++ steck.php
@@ -1,4 +1,5 @@
 <?php
+SESSION_START();
 // Anzeigen der CSV-Inhalte
 $datei = fopen("csvformular.csv", "r"); //Datei zum lesen öffnen
 $csv = array(); 						//anlegen eines Arrays
@@ -13,10 +14,13 @@ while(($data = fgetcsv($datei, 1000, ";")) !== FALSE) { //while-schleife zum bef
 
 foreach ($csv as list($csv2)) { //Auslesen der ersten Variable des jeweiligen Datensatzes
 //	foreach ($csv2 as $csv3) { //Auslesen des kompletten Arrays
+		$_SESSION["name"] = $csv2;
 		echo "HTML-CODE mit Logo und Verlinkung"; //Ausgeben eines Logos, Verlinkung auf Steckbrief
 		echo	"<p>$csv2</p>"; //Ausgeben der ersten Variable -> Name
 		
 	}
+	
+	
 
 //Ausgabe durch Ansprechen des entsprechenden Array-Indexes/ Erste Stelle bezieht sich auf den Datensatz/ zweite Stelle auf jeweilige Variable im Datensatz
 /*			echo "<p>Steckbrief</p>";
