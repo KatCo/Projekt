2a4925079b8dc95ab081f97df14a176346d41c77
IngaF <inga.fuhrmann@gmail.com>
2015-12-01 13:43:31 +0100

    Steckbrief
    Update der Kommentierung

--- steck.php
+++ steck.php
@@ -12,11 +12,13 @@ while(($data = fgetcsv($datei, 1000, ";")) !== FALSE) { //while-schleife zum bef
 			
 }
 
-foreach ($csv as list($csv2)) { //Auslesen der ersten Variable des jeweiligen Datensatzes
-//	foreach ($csv2 as $csv3) { //Auslesen des kompletten Arrays
-		$_SESSION["name"] = $csv2;
+foreach ($csv as list($csv2)) { 				//Auslesen der ersten Variable des jeweiligen Datensatzes
+//	foreach ($csv2 as $csv3) { 					//Auslesen des kompletten Arrays
+		$_SESSION['name'] = $csv2;				// Variable in Session speichern um sie auf Folgeseite wieder abrufen zu können
+												//Dort wird die gespeicherte Namenvariable mit den Namen in der Datenbank verglichen und
+												//anschließend soll der dazugehörige Datensatz wieder ausgegeben werden(Steckbriefseite)
 		echo "HTML-CODE mit Logo und Verlinkung"; //Ausgeben eines Logos, Verlinkung auf Steckbrief
-		echo	"<p>$csv2</p>"; //Ausgeben der ersten Variable -> Name
+		echo	"<p>$csv2</p>"; 				//Ausgeben der ersten Variable -> Name
 		
 	}
 	
