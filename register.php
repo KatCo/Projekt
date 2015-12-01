f7f5fb8dc571f7d23ed29b7b63482fd5965c5a1f
IngaF <inga.fuhrmann@gmail.com>
2015-12-01 18:29:59 +0100

    eingebundene HTML-Datei aktualisiert

--- register.php
+++ register.php
@@ -1,18 +1,18 @@
 <?php
 //Registrieren
-//doppelte Benutzernamenüberprüfung funktioniert noch nicht vollständig
+//doppelte BenutzernamenÃ¼berprÃ¼fung funktioniert noch nicht vollstÃ¤ndig
 //Variablen abfragen
 $name = $_POST["name"];
 $pass = $_POST["pass"];
 $pass2 = $_POST["pass2"];
 
-//Überprüfen, ob die eingetragenen Passwörter identisch sind
+//ÃœberprÃ¼fen, ob die eingetragenen PasswÃ¶rter identisch sind
 if ($pass == $pass2) {
-	$name_2 = array();	//Wenn sie identisch sind, überprüfen, ob Benutzername schon einmal verwendet wurde ==>leeres Array
+	$name_2 = array();	//Wenn sie identisch sind, Ã¼berprÃ¼fen, ob Benutzername schon einmal verwendet wurde ==>leeres Array
 
 	
-	$benutzerdatei = fopen ("benutzer.csv", "a+"); //öffnen der Datei mit den Benutzernamen
-	while (!feof($benutzerdatei)) {				// Datei wird Stück für Stück ausgelesen
+	$benutzerdatei = fopen ("benutzer.csv", "a+"); //Ã¶ffnen der Datei mit den Benutzernamen
+	while (!feof($benutzerdatei)) {				// Datei wird StÃ¼ck fÃ¼r StÃ¼ck ausgelesen
 		$zeile = fgets($benutzerdatei, 500);
 		$data = explode(";", $zeile);
 		array_push ($name_2, $benutzerdatei[0]);			//Der Benutzername wird ins Array abgelegt
@@ -20,12 +20,12 @@ if ($pass == $pass2) {
 	fclose($benutzerdatei); //Datei wird wieder geschlossen
 	
 	if (in_array($name, $name_2)) { //Ist der Benutzername schon vorhanden, erscheint ein Hinweis
-		echo "Der Benutzername ist leider schon vorhanden. Bitte gib´ einen neuen Namen ein!";
-		include ("form.html");
+		echo "Der Benutzername ist leider schon vorhanden. Bitte gibÂ´ einen neuen Namen ein!";
+		include ("reg.html");
 	}
 	else {
 		$eintrag = "$name".";"."$pass"; //Name und Passwort werden mit einem ";" getrennt und in eine Variable gespeichert
-		$benutzerdatei = fopen ("benutzer.csv", "a"); //Datei wird geöffnet
+		$benutzerdatei = fopen ("benutzer.csv", "a"); //Datei wird geÃ¶ffnet
 		fwrite($benutzerdatei, "$eintrag;\n"); //Name und Passwort wird in die Datei geschrieben
 		fclose($benutzerdatei); //Datei wird geschlossen
 		
@@ -34,8 +34,8 @@ if ($pass == $pass2) {
 	
 }
 else {
-	//Stimmen die Passwörter nicht überein, erscheint ein Hinweis
-	echo "Die eingegebenen Passwörter stimmen nicht überein. 
+	//Stimmen die PasswÃ¶rter nicht Ã¼berein, erscheint ein Hinweis
+	echo "Die eingegebenen PasswÃ¶rter stimmen nicht Ã¼berein. 
 			Bitte versuche es noch einmal!";
 			include("form.html");
 }
